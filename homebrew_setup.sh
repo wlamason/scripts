ec99da5f18b95ef9b61e88bdf45a86c0faad1edf
wlamason <lamasonian@gmail.com>
2023-10-22 15:58:28 -0400

    workaround for brew installed pyenv failing to build installed versions

--- homebrew_setup.sh
+++ homebrew_setup.sh
@@ -14,3 +14,11 @@ brew install pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+# https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+sudo apt update; sudo apt -y install build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev curl \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+# Install python3.12
+brew unlink pkg-config  # https://github.com/pyenv/pyenv/issues/2823#issuecomment-1769081965
+pyenv install 3.12
